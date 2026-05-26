9571daa750fd8bedf433a475ceb40688e2822ec9
Nishant Rajadhyaksha <79035403+nishant42491@users.noreply.github.com>
2026-05-26 14:48:24 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,78 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s2) < len(s1):
+            return False
+
+        count_map = [0 for i in range(26)]
+
+
+        for ch in s1:
+
+            ind = ord(ch) - 97
+            count_map[ind] += 1
+
+        
+        size = len(s1)
+
+        print(count_map)
+
+        for i in range(size):
+
+            ch = s2[i]
+
+            count_map[ord(ch) - 97] -= 1
+
+        if all(c == 0 for c in count_map):
+            return True
+
+        l = 0
+
+        r = size - 1
+
+        while r < len(s2):
+
+            print(count_map)
+            lchar = s2[l]
+
+            count_map[ord(lchar) - 97] += 1
+            l += 1
+
+            r+=1
+
+            if r == len(s2):
+                break
+            rchar = s2[r]
+            count_map[ord(rchar) - 97] -= 1
+
+            if all(c == 0 for c in count_map):
+                return True
+
+        
+        return False
+
+
+
+
+
+
+
+
+
+            
+
+        
+
+
+
+
+
+
+        
+
+
+        
+
+
+        
+        
